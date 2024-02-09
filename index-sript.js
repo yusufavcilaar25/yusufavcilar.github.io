63dd8dd8ccfeaa3df8cb8bafe2a47d1139217c2c
yusufavcilaar25 <159519004+yusufavcilaar25@users.noreply.github.com>
2024-02-09 14:45:08 +0300

    Add files via upload

---
+++ index-sript.js
@@ -0,0 +1,16 @@
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 5000;
+
+valueDisplays.forEach(valueDisplays => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplays.getAttribute
+    ("datval"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function(){
+        startValue += 1;
+        valueDisplays.textContent = startValue;
+        if(startValue == endValue){
+            clearInterval(counter)
+        }
+    }, duration);
+});
